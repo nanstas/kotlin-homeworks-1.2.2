9819d2fb69afad5f9cf7c15c289c30af6051be7b
Stanislav Nanoshkin <nanstas@yandex.ru>
2020-08-30 11:28:44 +0500

    changed the logic

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,10 @@
 package ru.netology
 
 fun main() {
-    val likes = 11
-    val ending = if (likes % 10 == 1
-        && likes != 11
-        && likes != 111
-        && likes != 1111
-        && likes != 11111) "человеку" else "людям"
+    val likes = 1
+    val likesStringLength = likes.toString().length
+    val penultimateSymbol = if (likesStringLength > 1) likes.toString()[likesStringLength - 2] else '0'
+    val ending = if (likes % 10 == 1 && penultimateSymbol != '1') "человеку" else "людям"
+
     println("Понравилось: $likes $ending")
 }
